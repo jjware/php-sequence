5c199a4013522344547a3160fd2a4cf4049af918
Joshua Jones <jjones@criticalmix.com>
2018-05-07 12:30:54 -0400

    add head, tail, and headOption tests

--- test/SequenceTest.php
+++ test/SequenceTest.php
@@ -20,4 +20,46 @@ class SequenceTest extends TestCase
     {
         $this->assertEquals(Sequence::cons(1, Sequence::nil()), new Cons(1, Sequence::nil()));
     }
+
+    public function testConsHead()
+    {
+        $s = Sequence::cons("a", Sequence::nil());
+        $this->assertEquals("a", $s->head());
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testNilHead()
+    {
+        $s = Sequence::nil();
+        $s->head();
+    }
+
+    public function testConsTail()
+    {
+        $s = Sequence::cons("a", Sequence::nil());
+        $this->assertEquals(Sequence::nil(), $s->tail());
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testNilTail()
+    {
+        $s = Sequence::nil();
+        $s->tail();
+    }
+
+    public function testConsHeadOption()
+    {
+        $s = Sequence::cons("a", Sequence::nil());
+        $this->assertEquals(Option::some("a"), $s->headOption());
+    }
+
+    public function testNilHeadOption()
+    {
+        $s = Sequence::nil();
+        $this->assertEquals(Option::none(), $s->headOption());
+    }
 }
